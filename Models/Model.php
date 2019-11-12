e92a1fd391b496355dc2cc151596f1709f285fc2
Stephen Abrokwah <AbrokwahS@gmail.com>
2019-11-12 15:28:09 -0500

    monolog,phpunit var-dumper

---
+++ Models/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjabrok
+ * Date: 11/12/19
+ * Time: 3:12 PM
+ */
+
+namespace Model;
+
+
+class Model
+{
+
+}
---
+++ Views/View.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sjabrok
+ * Date: 11/12/19
+ * Time: 3:07 PM
+ */
+
+namespace View;
+
+
+class View
+{
+
+}
